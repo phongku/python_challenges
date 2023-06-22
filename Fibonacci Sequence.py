bd44c0968eb3e0788cdcc660602956d15bf30958
phongku <etrin00azn@gmail.com>
2023-06-22 12:27:30 -0700

    Create Fibonacci Sequence.py

---
+++ Fibonacci Sequence.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    if n < 0:
+        return -1
+    elif n < 2:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+if __name__ == '__main__':
+    start_num = int(input())
+    print(f'fibonacci({start_num}) is {fibonacci(start_num)}')
