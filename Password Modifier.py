c2c40d99d93fe9893386e47fc3f2436e6fa2d8e4
phongku <etrin00azn@gmail.com>
2023-06-22 12:23:24 -0700

    Create Password Modifier.py

---
+++ Password Modifier.py
@@ -0,0 +1,12 @@
+word = input()
+password = ''
+
+word = word.replace('i','1')
+word = word.replace('a','@')
+word = word.replace('m','M')
+word = word.replace('B','8')
+word = word.replace('s','$')
+
+word = word + '!'
+
+print(word)
